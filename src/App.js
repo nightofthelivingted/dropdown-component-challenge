95e31612d906159bf0859e13ae15082b64a37d89
Amy Russell <amy.russell@elationhealth.com>
2021-02-18 15:40:51 -0700

    Add skeleton, design, and instructions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+const groupOptions = ["singers", "actors", "athletes"]
+const singerOptions = ["beyonce", "bob dylan", "taylor swift", "prince"]
+const actorOptions = ["will smith", "kristen bell", "daniel radcliffe", "eva longoria"]
+const athleteOptions = ["payton manning", "steph curry", "serena williams", "tiger woods"]
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
